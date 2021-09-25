5b3eeed26a9a96de15d6db96cbf333486f9722c6
unknown <alexm1397@gmail.com>
2021-09-25 12:23:50 -0500

    added fetch formatting

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,10 +1,14 @@
 
-
-
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function(user) {
+    //formats the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+    //request to url
+    fetch(apiUrl).then(function(response) {
+    response.json().then(function(data) {
+        console.log(data)
+        });
+    });
 };
-
 
 
 getUserRepos();
