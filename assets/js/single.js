e692311289096b99ab7ceef2569df560bcf1de6e
unknown <alexm1397@gmail.com>
2021-09-25 15:24:07 -0500

    single repo issue page base functions added

---
+++ assets/js/single.js
@@ -0,0 +1,44 @@
+
+var issueContainerEl = document.querySelector("#issues-container");
+
+var getRepoIssues = function(repo) {
+    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayIssues(data);
+            });
+        } else { 
+            alert("There was a problem with your request!");
+        }
+    });
+};
+
+var displayIssues = function(issues) {
+    if (issues.length === 0) {
+        issueContainerEl.textContent = "This repo has no open issues!";
+        return;
+    }
+    for (var i = 0; i < issues.length; i++) {
+        //create a link el to issue on github
+        var issueEl = document.createElement("a");
+        issueEl.classList = "list-item flex-row justify-space-between align-center";
+        issueEl.setAttribute("href", issues[i].html_url);
+        issueEl.setAttribute("target", "_blank");
+        issueContainerEl.appendChild(issueEl);
+        //create span
+        var titleEl = document.createElement("span");
+        titleEl.textContent = issues[i].title;
+        issueEl.appendChild(titleEl);
+        var typeEl = document.createElement("span");
+        //check if issue is issue, or pull req
+        if (issues[i].pull_request) {
+            typeEl.textContent = "(Pull request)";
+        } else { 
+            typeEl.textContent = "(Issue)";
+        }
+        issueEl.appendChild(typeEl);
+    }
+};
+
+getRepoIssues("facebook/react");
