576cf7c04950074ade123eda5c0bbdf46893c98b
unknown <alexm1397@gmail.com>
2021-09-27 19:46:42 -0500

    added error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -17,7 +17,7 @@ var getRepoIssues = function(repo) {
                 }
             });
         } else { 
-            alert("There was a problem with your request!");
+            document.location.replace("./index.html");
         }
     });
 };
@@ -61,8 +61,12 @@ var displayWarning = function(repo) {
 var getRepoName = function() {
     var queryString = document.location.search;
     var repoName = queryString.split("=")[1];
-    getRepoIssues(repoName);
-    repoNameEl.textContent = repoName;
-}
+    if (repoName) {
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else { 
+        document.location.replace("./index.html");
+    }
+};    
 
 getRepoName();
